d9f51f55bdf4b138451ce8adb1c2f31f06865002
Ethan Simpson <ethansown@gmail.com>
2018-11-21 17:12:04 -0600

    Created Project Delete PHP

--- PHP_Files/deleteproject.php
+++ PHP_Files/deleteproject.php
@@ -9,6 +9,31 @@
     echo "<h1>Delete Project Form</h1>";
 ?>
 <?php
+    $db_server = mysql_connect('localhost', 'ethan', 'kokoko345543!');
+    if(!$db_server)
+        die("Unable to connect to MySQL: " . mysql_error());
+    if(!mysql_select_db('ethan_RYO'))
+        die('Unable to select database: ' . mysql_error());
+    $query = "SELECT ProjectName, StartDate, EndDate, DEVELOPER.DeveloperName, PUBLISHER.PublisherName FROM PUBLISHER, DEVELOPER, GAME_PROJECT WHERE DEVELOPER.PublisherName = PUBLISHER.PublisherName AND GAME_PROJECT.DeveloperName = DEVELOPER.DeveloperName";
+
+    $result = mysql_query($query);
+    if(!result)
+        die("Unable to execute query: " . mysql_error());
+    
+    echo "<table border=\"1\">";
+    echo "<tr><th>ProjectName</th><th>StartDate</th><th>EndDate</th><th>DeveloperName</th><th>PublisherName</th></tr>\n";
+
+    $rows = mysql_num_rows($result);
+    for($r = 0; $r < $rows; $r++){
+        echo "<tr>";
+        for($c = 0; $c < 5; $c++){
+            echo "<td>" . mysql_result($result, $r, $c) . "</td>";
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+?>
+<?php
     if(isset($_GET['projectname'])){
         $db_server = mysql_connect('localhost', 'ethan', 'kokoko345543!');
         if(!$db_server)
